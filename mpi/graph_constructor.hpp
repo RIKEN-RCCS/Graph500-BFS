RIKEN-RCCS/Graph500-BFS

commit f2a42cced6b3b25a6a1a3b9f93cc1dd910cdeaa7

insert malloc_trim after memory delete points

mpi/graph_constructor.hpp
@@ -9,6 +9,7 @@
 #define GRAPH_CONSTRUCTOR_HPP_
 
 #include <sys/mman.h>
+#include <malloc.h>
 
 #include "parameters.h"
 #include "limits.h"
@@ -308,8 +309,10 @@ struct DegreeCalculation {
 
   GraphConstructionData process() {
     int64_t num_verts = num_orig_local_verts();
-    int64_t* degree = static_cast<int64_t*>(
-        cache_aligned_xcalloc(num_verts * sizeof(int64_t)));
+    auto page_size = sysconf(_SC_PAGESIZE);
+    size_t degree_bytes = (num_verts * sizeof(int64_t) + page_size - 1) / page_size * page_size;
+    int64_t* degree = static_cast<int64_t*>(page_aligned_xmalloc(degree_bytes));
+    madvise(degree, degree_bytes, MADV_DONTNEED);
     LocalVertex* reorder_map = calc_degree(degree);
     make_construct_data(reorder_map);
     return gather_data(reorder_map, degree);
@@ -424,6 +427,7 @@ struct DegreeCalculation {
     // free memory
     delete[] dwide_row_data_;
     dwide_row_data_ = NULL;
+    malloc_trim(0);
 
     // allocate 2
     wide_row_length_ = static_cast<int64_t*>(
@@ -700,6 +704,7 @@ class GraphConstructor2DCSR {
 
     delete degree_calc_;
     degree_calc_ = NULL;
+    malloc_trim(0);
   }
 
   // function #1
@@ -1369,6 +1374,7 @@ class GraphConstructor2DCSR {
 
       if (mpi.isMaster())
         print_with_prefix("Iteration %d finished.", loop_count);
+      malloc_trim(0);
     }
 
     edge_list->endRead();

commit f232f8a1fbbd7fee5b4e075c18ea5fe9273d61db

fix: remove madvise(2) since it fails on Fugaku
f866357 and f2a42cc introduced `madvise` with `MADV_DONTNEED` for
filling pages with zeros without touching (demanding) those pages.
They are considered to have an effect to reduce the peak memory
consumption. However, it has turned out that `madvise` fails (returns
non-zero) on Fugaku, and allocated pages are not zero-filled. Hence,
this commit reverts pieces of code around `madvise` to those before
f866357 and f2a42cc.

Be careful that this commit may increases the peak memory consumption.
It is needed to investigate the amount of consumed memory before
large-scale execution.

mpi/graph_constructor.hpp
@@ -9,7 +9,6 @@
 #define GRAPH_CONSTRUCTOR_HPP_
 
 #include <cstring>
-#include <sys/mman.h>
 #include <malloc.h>
 
 #include "parameters.h"
@@ -350,11 +349,8 @@ struct DegreeCalculation {
 
   GraphConstructionData process() {
     int64_t num_verts = num_orig_local_verts();
-    auto page_size = sysconf(_SC_PAGESIZE);
-    size_t degree_bytes =
-        (num_verts * sizeof(int64_t) + page_size - 1) / page_size * page_size;
-    int64_t* degree = static_cast<int64_t*>(page_aligned_xmalloc(degree_bytes));
-    madvise(degree, degree_bytes, MADV_DONTNEED);
+    int64_t* degree = static_cast<int64_t*>(
+        cache_aligned_xcalloc(num_verts * sizeof(int64_t)));
     LocalVertex* reorder_map = calc_degree(degree);
     make_construct_data(reorder_map);
     return gather_data(reorder_map, degree);
@@ -1359,18 +1355,10 @@ class GraphConstructor2DCSR {
         xMPI_Alloc_mem(2 * EdgeList::CHUNK_SIZE * sizeof(EdgeType)));
 
     // const int64_t num_local_verts = g.num_local_verts_;
-    auto page_size = sysconf(_SC_PAGESIZE);
-    auto edge_array_bytes =
-        (wide_row_starts_[num_wide_rows_] * sizeof(int64_t) + page_size - 1) /
-        page_size * page_size;
-    auto src_vert_bytes =
-        (wide_row_starts_[num_wide_rows_] * sizeof(uint16_t) + page_size - 1) /
-        page_size * page_size;
-    g.edge_array_ = (int64_t*)page_aligned_xmalloc(edge_array_bytes);
-    src_vertexes_ = (uint16_t*)page_aligned_xmalloc(src_vert_bytes);
-    // zero clear (need...?)
-    madvise(g.edge_array_, edge_array_bytes, MADV_DONTNEED);
-    madvise(src_vertexes_, src_vert_bytes, MADV_DONTNEED);
+    g.edge_array_ = (int64_t*)cache_aligned_xcalloc(
+        wide_row_starts_[num_wide_rows_] * sizeof(int64_t));
+    src_vertexes_ = (uint16_t*)cache_aligned_xcalloc(
+        wide_row_starts_[num_wide_rows_] * sizeof(uint16_t));
 
     int num_loops = edge_list->beginRead(true);
 